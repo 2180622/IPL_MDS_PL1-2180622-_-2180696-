8f84d0af274b733e3f966407ca0cef9720199b63
2180622 <45967854+2180622@users.noreply.github.com>
2019-04-30 11:01:38 +0100

    Add files via upload
    Commit da aplicação

---
+++ Aplicação/StandAutomoveis/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace StandAutomoveis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha a sua opção";
+            // 
+            // button4
+            // 
+            this.button4.Image = global::StandAutomoveis.Properties.Resources.GestaoVendas;
+            this.button4.Location = new System.Drawing.Point(670, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 160);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Gestão de Vendas";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::StandAutomoveis.Properties.Resources.GestaoAluguer;
+            this.button3.Location = new System.Drawing.Point(468, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 160);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Gestão de Aluguer";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::StandAutomoveis.Properties.Resources.GestaoOficina;
+            this.button2.Location = new System.Drawing.Point(271, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 160);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Gestão de Oficina";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::StandAutomoveis.Properties.Resources.GestaoClientes;
+            this.button1.Location = new System.Drawing.Point(73, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 160);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Gestão de Clientes";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 361);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
